ed1b38307b91f2149a290c8112f11610b634ca3e
adam huang <937162905@qq.com>
2017-09-17 20:31:20 +0800

    alter index.js

--- beginning/school/index.js
+++ beginning/school/index.js
@@ -1,3 +1,11 @@
 var banji = require('./class') 
 
-banji.add('Zhang', ['HUANG', 'LIU', 'LI'])
+exports.add = function(banjies) {
+    banjies.forEach(function(item, index) {
+        var _banji = item
+        var teacherName = item.teacherName
+        var students = item.students
+        
+        banji.add(teacherName, students)
+    })
+} 
